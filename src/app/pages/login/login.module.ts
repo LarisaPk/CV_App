d0425de7be70d426bcb9cdff9ec1e31a4c5ab75a
Larisa Pyykölä <Larisa.pyykola@gmail.com>
2019-03-23 15:16:16 +0200

    added content

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
+//import { Facebook } from '@ionic-native/facebook';
 
 import { IonicModule } from '@ionic/angular';
 
